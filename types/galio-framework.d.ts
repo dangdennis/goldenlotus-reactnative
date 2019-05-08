d383763c946cc9fdc1bfdf5d30042ccda82039bc
Dennis Dang <dangggdennis@gmail.com>
2019-05-08 14:34:33 -0700

    More types

--- types/galio-framework.d.ts
+++ types/galio-framework.d.ts
@@ -1,33 +1,33 @@
-declare module 'galio-framework' {
-  import * as React from 'react';
+// declare module 'galio-framework' {
+//   import * as React from 'react';
 
-  export interface BlockProps {
-    children?: (false | Element)[];
-    row?: boolean;
-    flex?: boolean | number;
-    center?: boolean;
-    middle?: boolean;
-    top?: boolean;
-    bottom?: boolean;
-    right?: boolean;
-    card?: boolean;
-    left?: boolean;
-    shadow?: boolean;
-    space?: string;
-    fluid?: boolean;
-    height?: number;
-    width?: number;
-    shadowColor?: string;
-    safe?: boolean;
-    styles?: any;
-    theme?: any;
-  }
+//   export interface BlockProps {
+//     children?: (false | Element)[];
+//     row?: boolean;
+//     flex?: boolean | number;
+//     center?: boolean;
+//     middle?: boolean;
+//     top?: boolean;
+//     bottom?: boolean;
+//     right?: boolean;
+//     card?: boolean;
+//     left?: boolean;
+//     shadow?: boolean;
+//     space?: string;
+//     fluid?: boolean;
+//     height?: number;
+//     width?: number;
+//     shadowColor?: string;
+//     safe?: boolean;
+//     styles?: any;
+//     theme?: any;
+//   }
 
-  export class Block extends React.Component<BlockProps> {}
+//   export class Block extends React.Component<BlockProps> {}
 
-  export interface GalioProviderProp {
-    children?: React.ReactElement;
-    theme?: any;
-  }
-  export class GalioProvider extends React.Component<GalioProviderProp> {}
-}
+//   export interface GalioProviderProp {
+//     children?: React.ReactElement;
+//     theme?: any;
+//   }
+//   export class GalioProvider extends React.Component<GalioProviderProp> {}
+// }
